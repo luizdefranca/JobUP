2bbe635dd954ef8e5521c15821574cf9d03fe0a8
dhiogoacioli <dhiogoacioli@gmail.com>
2017-04-17 00:17:17 -0300

    implementação de API para logar

---
+++ WEB/JOB.WEB/ApiController/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity.Owin;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+
+namespace JOB.WEB.ApiController
+{
+    [AllowAnonymous]
+    public class AccountController : System.Web.Http.ApiController
+    {
+        public ApplicationSignInManager SignInManager => HttpContext.Current.GetOwinContext().Get<ApplicationSignInManager>();
+        public ApplicationUserManager UserManager => HttpContext.Current.GetOwinContext().GetUserManager<ApplicationUserManager>();
+
+        public async Task<HttpResponseMessage> Get(string Email, string Password)
+        {
+            var user = await UserManager.FindByNameAsync(Email);
+            if (user != null)
+            {
+                if (!await UserManager.IsEmailConfirmedAsync(user.Id))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new HttpError("You must have a confirmed email to log on. The confirmation token has been resent to your email account."));
+                }
+            }
+
+            var result = await SignInManager.PasswordSignInAsync(Email, Password, false, shouldLockout: true);
+            switch (result)
+            {
+                case SignInStatus.Success:
+                    return Request.CreateResponse(HttpStatusCode.OK, "Success");
+
+                case SignInStatus.LockedOut:
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new HttpError("LockedOut"));
+
+                case SignInStatus.RequiresVerification:
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new HttpError("RequiresVerification"));
+
+                case SignInStatus.Failure:
+                default:
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new HttpError("Failure"));
+            }
+        }
+    }
+}
--- WEB/JOB.WEB/Global.asax.cs
+++ WEB/JOB.WEB/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using JOB.WEB.App_Start;
+﻿using System.Web.Http;
+using JOB.WEB.App_Start;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
@@ -11,6 +12,7 @@ namespace JOB.WEB
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
