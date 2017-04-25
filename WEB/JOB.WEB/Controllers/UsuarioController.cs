789ddf87b1d94691ddd5c81132583e75777cd2af
dhiogoacioli <dhiogoacioli@gmail.com>
2017-04-24 21:27:14 -0300

    ..

--- WEB/JOB.WEB/Controllers/UsuarioController.cs
+++ WEB/JOB.WEB/Controllers/UsuarioController.cs
@@ -1,7 +1,5 @@
 ﻿using AutoMapper;
 using JOB.DATA;
-using JOB.DATA.Domain;
-using JOB.DATA.ValueObject;
 using JOB.WEB.Models;
 using JOB.WEB.Validation;
 using System;
@@ -40,7 +38,7 @@ namespace JOB.WEB.Controllers
         }
 
         // GET: Usuario/Details/5
-        public async Task<ActionResult> Details(Guid id)
+        public async Task<ActionResult> Aprovar(Guid id)
         {
             var domain = await ctx.Usuario
                 .Include(i => i.CONTATO)
@@ -50,60 +48,6 @@ namespace JOB.WEB.Controllers
 
             return View(model);
         }
-
-        //// GET: Usuario/Edit/5
-        //public async Task<ActionResult> Edit(Guid id)
-        //{
-        //    var domain = await ctx.Usuario
-        //        .Include(i => i.CONTATO)
-        //        .Include(i => i.ENDERECO)
-        //        .FirstAsync(w => w.ID_USUARIO == id);
-
-        //    var model = Mapper.Map<UsuarioViewModel>(domain); //converte a classe original para o viewmodel (que é reconhecida pela view)
-
-        //    return View(model);
-        //}
-
-        //// POST: Usuario/Edit/5
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<ActionResult> Edit(Guid id, UsuarioViewModel obj)
-        //{
-        //    if (!ModelState.IsValid) return View(obj);
-
-        //    try
-        //    {
-        //        var domain = await ctx.Usuario
-        //            .Include(i => i.CONTATO)
-        //            .Include(i => i.ENDERECO)
-        //            .FirstAsync(w => w.ID_USUARIO == id);
-
-        //        domain.AtualizaDados(obj.NOME, new CPF(obj.CPF), new RG(obj.RgUF, obj.RgNR), obj.DT_NASCIMENTO);
-
-        //        if (domain.CONTATO == null)
-        //            domain.AdicionarContato(new Telefone(obj.ContatoFIXO), new Telefone(obj.ContatoCELULAR), new Email(obj.ContatoEMAIL));
-        //        else
-        //            domain.CONTATO.AtualizarValor(new Telefone(obj.ContatoFIXO), new Telefone(obj.ContatoCELULAR));
-
-        //        if (domain.ENDERECO == null)
-        //            domain.AdicionarEndereco(obj.EnderecoUF, obj.EnderecoCEP, obj.EnderecoLOGRADOURO, obj.EnderecoCOMPLEMENTO, obj.EnderecoBAIRRO, obj.EnderecoCIDADE);
-        //        else
-        //            domain.ENDERECO.AtualizaValores(obj.EnderecoUF, obj.EnderecoCEP, obj.EnderecoLOGRADOURO, obj.EnderecoCOMPLEMENTO, obj.EnderecoBAIRRO, obj.EnderecoCIDADE);
-
-        //        ctx.Entry(domain).State = EntityState.Modified;
-        //        ctx.Entry(domain.CONTATO).State = EntityState.Modified;
-        //        ctx.Entry(domain.ENDERECO).State = EntityState.Modified;
-
-        //        await ctx.SaveChangesAsync();
-
-        //        return RedirectToAction("Index");
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        ModelState.AddModelError("", ex.TratarMensagem());
-        //        return View(obj);
-        //    }
-        //}
 
         // GET: Usuario/Delete/5
         public async Task<ActionResult> Delete(Guid id)
@@ -132,17 +76,6 @@ namespace JOB.WEB.Controllers
                 ModelState.AddModelError("", ex.TratarMensagem());
                 return View(obj);
             }
-        }
-
-        public async Task<ActionResult> Aprovar(Guid id)
-        {
-            var domain = await ctx.Usuario.FirstAsync(w => w.ID_USUARIO == id);
-
-            domain.Aprovar();
-            ctx.Entry(domain).State = EntityState.Modified;
-            await ctx.SaveChangesAsync();
-
-            return RedirectToAction("Index");
         }
 
         public async Task<ActionResult> Ativar(Guid id)
