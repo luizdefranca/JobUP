a43e61c62ee3042c2734848352336f7a06f5e397
dhiogoacioli <dhiogoacioli@gmail.com>
2017-05-14 01:34:14 -0300

    ..

--- WEB/JOB.DATA/Migrations/Configuration.cs
+++ WEB/JOB.DATA/Migrations/Configuration.cs
@@ -1,20 +1,12 @@
 namespace JOB.DATA.Migrations
 {
     using System.Data.Entity.Migrations;
-    using System.Linq;
 
     internal sealed class Configuration : DbMigrationsConfiguration<JOB.DATA.Contexto>
     {
         public Configuration()
         {
             AutomaticMigrationsEnabled = false;
-        }
-
-        protected override void Seed(JOB.DATA.Contexto context)
-        {
-            if (context.Usuario.Any()) return;
-
-            BasicoSeed.Seed(context);
         }
     }
 }
