b3013a4b360b15a61d73621ed34bde732b228e0e
dhiogoacioli <dhiogoacioli@gmail.com>
2017-06-07 07:11:29 -0300

    novas apis

---
+++ WEB/JOB.API/Controllers/ServicoPrivadoClienteController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using JOB.DATA;
+using JOB.WEB.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace JOB.API.Controllers
+{
+    public class ServicoPrivadoClienteController : ApiController
+    {
+        private readonly Contexto ctx = new Contexto();
+
+        public HttpResponseMessage Get(Guid idUsuarioCliente)
+        {
+            //TODO: nao tem que ser privado. tem q ser qualquer um
+            var domain = ctx.Servico.Where(w => w.ID_USUARIO == idUsuarioCliente & w.PUBLICO != false).ToList();
+
+            var lstModel = Mapper.Map<List<ServicoViewModel_api>>(domain);
+
+            //ClaimsIdentity claimIdenties = HttpContext.User.Identity as ClaimsIdentity;
+
+            //ViewBag.POSSUI_FACE = claimIdenties.FindFirst("FacebookAccessToken") != null;
+
+            foreach (var model in lstModel)
+            {
+                model.NOME = ctx.Usuario.First(f => f.ID_USUARIO == model.ID_USUARIO).NOME;
+                model.DESC_ESPECIALIDADE = ctx.Especialidade.First(f => f.ID_ESPECIALIDADE == model.ID_ESPECIALIDADE).DESCRICAO;
+
+                if (model.ID_SUB_ESPECIALIDADE != null && model.ID_SUB_ESPECIALIDADE != 0)
+                {
+                    model.DESC_SUB_ESPECIALIDADE = ctx.SubEspecialidade.First(f => f.ID_SUB_ESPECIALIDADE == model.ID_SUB_ESPECIALIDADE).DESCRICAO;
+                }
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, lstModel);
+        }
+    }
+}
