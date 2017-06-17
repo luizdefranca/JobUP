604bd863d928d13019cc6a1e0ca534b3f8997039
emersonpmpe <emersonsilva.pmpe@gmail.com>
2017-06-17 11:05:59 -0300

    Novo comitt

--- MOBILE2/JobUp/app/src/main/java/com/br/jobup/activities/ListaPropostaActivity.java
+++ MOBILE2/JobUp/app/src/main/java/com/br/jobup/activities/ListaPropostaActivity.java
@@ -2,21 +2,32 @@ package com.br.jobup.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 import com.br.jobup.R;
+import com.br.jobup.adapters.PropostaAdapter;
+import com.br.jobup.models.servico.Proposta;
 
-/*
- * Created by Luiz Carlos Ramos on 15/06/17 19:43
- *
- * Copyright (c) 2017. All rights reserved.
- * Last modified 15/06/17 19:43
- */
+import java.util.List;
+
 
 public class ListaPropostaActivity extends AppCompatActivity {
+
+    private ListView mListPropostas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_proposta);
+
+        mListPropostas = (ListView) findViewById(R.id.lVPropostas);
+    }
+
+    private void carregaListaPropostas(){
+        IPropostaDao dao = new PropostaDao(this);
+        List<Proposta> propostas = dao.getAll();
+        dao.close();
+        PropostaAdapter adapter = new PropostaAdapter(this, propostas);
+        mListPropostas.setAdapter(adapter);
     }
 }
